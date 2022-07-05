47bd2f612a63c50c535499d60cd7e056d24de627
Akshay Dhayal <akshaydhayalkd99@gmail.com>
2022-07-05 15:43:23 +0530

    Bootstrap added

--- socialfrontend/src/components/App/App.js
+++ socialfrontend/src/components/App/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {Button} from 'react-bootstrap';
+import NavbarComponent from './Navbar/NavbarComponent';
 
 function App() {
   return (
     <div className="App">
+
+      <NavbarComponent/>
       <h1>Hello World!!!</h1>
+      <Button variant="danger">Danger</Button> 
+      <Button variant="info">Info</Button>
+
     </div>
   );
 }
