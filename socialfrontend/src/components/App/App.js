1a4f54cd3359d8fbfeb0c70c0b802af74580e818
Akshay Dhayal <akshaydhayalkd99@gmail.com>
2022-07-05 16:13:36 +0530

    minor changes

--- socialfrontend/src/components/App/App.js
+++ socialfrontend/src/components/App/App.js
@@ -5,16 +5,7 @@ import NavbarComponent from './Navbar/NavbarComponent';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-
-      <NavbarComponent/>
-      <h1>Hello World!!!</h1>
-      <Button variant="danger">Danger</Button> 
-      <Button variant="info">Info</Button>
-
-=======
       <h1>Hello World2!!!</h1>
->>>>>>> 8bee5cee8c2878611fd881e834f042f21a6a72d7
     </div>
   );
 }
